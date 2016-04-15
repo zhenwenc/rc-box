59db02c03a253150a50fbbf7a86dd0f59f64a39a
Frederick Cai <frederick.cai@gmail.com>
2016-04-15 21:28:54 +1200

    Minor improve error message

--- src/core/TablePluginBase.ts
+++ src/core/TablePluginBase.ts
@@ -9,7 +9,6 @@ export abstract class TablePluginBase implements TablePlugin {
   // Will be defined in register method
   private updateCallback: StateUpdateCallback
 
-
   register(updateCallback: StateUpdateCallback) {
     check(!this.updateCallback, `Unexpected method call: ` +
       `'register' method should only be called internally!`)
@@ -20,7 +19,8 @@ export abstract class TablePluginBase implements TablePlugin {
     check(this.updateCallback, `Unexpected method call: ` +
       `You have stateful plugin in your data table, but there is ` +
       `no plugin state update callback function. ` +
-      `See DataTableProps#onStateUpdate property for more details.`)
+      `See DataTableProps#onStateUpdate property for more details. ` +
+      `Please check implementation of ${this.constructor.name}.`)
     if (this.updateCallback)
       this.updateCallback(this.constructor.name, this)
   }
@@ -28,5 +28,3 @@ export abstract class TablePluginBase implements TablePlugin {
   abstract get priority(): number
   abstract process(data: TableData, columns: List<ColumnDef>): TableData
 }
-
-export default TablePluginBase
--- src/utils/check.ts
+++ src/utils/check.ts
@@ -1,6 +1,6 @@
 export function check(target: any, message: string) {
   if (!target) {
-    throw new Error(`[RCBOX] ${message}.`)
+    throw new Error(`[RCBOX] ${message}`)
   }
   return target
 }
